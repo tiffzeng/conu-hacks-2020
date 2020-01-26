87bc3e759903515f468e5561e3639015762333ac
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2020-01-26 07:01:16 -0500

    adding front end player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,18 @@ import Footer from './Components/Footer';
 import VideoPlayer from './Components/VideoPlayer';
 import {getSong} from './api/Search';
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      playing: false,
+      isLoading: false,
+      urls: [],
       id: "",
       data: [],
-      videoUrl: "https://ak4.picdn.net/shutterstock/videos/32074804/thumb/stock-footage-large-container-ship-at-sea-top-down-aerial.mp4",
+      videoUrl: "",//"https://ak4.picdn.net/shutterstock/videos/32074804/thumb/stock-footage-large-container-ship-at-sea-top-down-aerial.mp4",
       imageUrl:
         ""//'https://image.shutterstock.com/display_pic_with_logo/2723875/755022088/stock-photo-aerial-view-to-ocean-waves-blue-water-background-755022088.jpg'
     };
@@ -25,24 +29,36 @@ class App extends React.Component {
     console.log(this.state.data);
   }
 
+
   async getID(id){
-    this.setState({id: id});
+    this.setState({
+      isLoading: true
+    })
     //get song info
     const response = await getSong(id);
-    console.log(response);
+    this.setState({
+      id: id,
+      urls: response,
+      isLoading: false
+    })
+
+    this.displayVideo();
+    console.log(response.data[0].duration);
   }
 
+  
   displayVideo() {
-    return (
-      <div>
-      {this.state.videoUrl ? (
-          <VideoPlayer url={this.state.videoUrl} />
-        ) : (
-          this.state.imageUrl && <img src={this.state.imageUrl} alt="new" width="640" height="360"/>
-        )}
-  </div>
-    );
+    for (let i=0; i<this.state.urls.length-2; i++) {
+      setTimeout( function timer(){
+          this.setState({
+            videoUrl: this.state.urls[i]
+          }).then(() => {
+            this.forceUpdate();
+          })
+      }, i*4000 );
   }
+  }
+  
 
   displayList(){
     return(
@@ -62,7 +78,9 @@ class App extends React.Component {
           <SearchBar passData={this.passData} />
 
           <br />
-          {this.state.id ? this.displayVideo() : this.displayList()}
+          {this.state.isLoading && <div>Loading</div>}
+          {!this.state.id && this.displayList()}
+          <ReactPlayer url={this.state.videoUrl} playing={this.state.playing}/>
           <Footer />
         </header>
       </div>
