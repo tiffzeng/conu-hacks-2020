d868286a0e40eb4553cb459f394bdcb3f0c454ed
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-01-26 02:05:55 -0500

    shutterstock logic implemented

--- backend/controller/controllerLogic.js
+++ backend/controller/controllerLogic.js
@@ -3,12 +3,14 @@ const getLyrics = require('./getLyrics.js');
 const getMetadata = require('./getMetadata.js');
 const lyricMatching = require('./lyricMatching.js');
 
-getMetadata(1372807).then((data) => {
-  querySearcher(data['artist'], data['title']).then((response) => {
-    getLyrics(response).then((lyrics) => {
-      lyricMatching(lyrics, data['duration']).then((res) => {
-        console.log(res);
+module.exports = async (songId) => {
+  getMetadata(1372807).then((data) => {
+    querySearcher(data['artist'], data['title']).then((response) => {
+      getLyrics(response).then((lyrics) => {
+        lyricMatching(lyrics, data['duration']).then((res) => {
+          console.log(res);
+        });
       });
     });
   });
-});
+};
